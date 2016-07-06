40c2b9abb09e6c0acab6cb580ab5f7ebe9872dcb
Daniel <djs66256@163.com>
2016-07-06 20:14:50 +0800

    Initial Commit

---
+++ DDAttachmentTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDAttachmentTextView
+//
+//  Created by hzduanjiashun on 16/7/6.
+//  Copyright © 2016年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDAttachmentTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDAttachmentTextView
+//
+//  Created by hzduanjiashun on 16/7/6.
+//  Copyright © 2016年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
